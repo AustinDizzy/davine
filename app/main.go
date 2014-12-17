365c768c419bc4995613c41b5d86656382d8119d
Austin Dizzy <dizzy@wow.com>
2014-12-17 07:01:32 +0000

    Added mustache templates and User(Fetch|Store) handlers.

--- app/main.go
+++ app/main.go
@@ -4,15 +4,26 @@ import (
     "fmt"
     "net/http"
     "github.com/gorilla/mux"
+    "github.com/hoisie/mustache"
+    "path"
+    "os"
 )
 
 func init() {
     router := mux.NewRouter()
-    router.HandleFunc("/user", handler).Methods("GET")
+    router.HandleFunc("/user", UserStoreHandler).Methods("GET")
+    router.HandleFunc("/u/{user}", UserFetchHandler).Methods("GET")
     
     http.Handle("/", router)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func UserFetchHandler(w http.ResponseWriter, r *http.Request) {
+    template := path.Join(path.Join(os.Getenv("PWD"), "templates"), "message.html.mustache")
+    vars := mux.Vars(r)
+    data := mustache.RenderFile(template, map[string]string{"message": "Hello, " + vars["user"]})
+    fmt.Fprint(w, data)
+}
+
+func UserStoreHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprint(w, "Hello, world!")
 }
