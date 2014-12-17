0bbb414dc2fcc2447425f8ca291763e701fd8b3b
Austin Dizzy <dizzy@wow.com>
2014-12-17 21:58:33 +0000

    Added Vine fields to be stored.

--- app/types.go
+++ app/types.go
@@ -8,3 +8,45 @@ type QueuedUser struct {
 	UserID     string
 	Discovered time.Time
 }
+
+type StoredUserMeta struct {
+    UserID  int64
+    Username    string
+    Location string
+    Description string
+    Backgroudn string
+    Previous struct{
+        Usernames []struct {
+           Username string
+           Discovered   time.Time
+        }
+        Location []struct {
+            Location string
+            Discovered  time.Time
+        }
+        Description []struct {
+            Description string
+            Discovered  time.Time
+        }
+        Background  []struct {
+            Background string
+            Discovered time.Time
+        }
+    }
+    VanityUrl   string
+    Verified    bool
+    VerifiedDate    time.Time
+    AvatarUrl   string
+}
+
+type StoredUserData struct {
+    UserID int64
+    LastUpdated time.Time
+    Followers []int64
+    Following []int64
+    Loops []int64
+    AuthoredPostCount []int64
+    PostCount []int64
+    Likes []int64
+    Updated []time.Time
+}
