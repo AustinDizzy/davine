47ee5ee411a1fc497d8c9a7dcb92bf41889034dc
Austin Dizzy <dizzy@wow.com>
2014-12-27 16:38:29 +0000

    Consolidated inline style/script on pages.
    Also added fixed header/logo fade animation,
    forgot to commit when originally made. Oops.

---
+++ public/js/main.js
@@ -0,0 +1,25 @@
+$(function(){
+
+    $('.formatInt').each(function(i) {
+        val = $('.formatInt')[i].innerText;
+        str = numeral(parseInt(val));
+        $('.formatInt')[i].innerText = str.format($(this).data("format"));
+    });
+
+    var menu = $('#main-menu');
+    var origOffsetY = menu.offset().top;
+
+    function scroll() {
+        if ($(window).scrollTop() >= origOffsetY) {
+            $('#main-menu').addClass('navbar-fixed-top');
+            $('.main').css({"padding-top": "72px"});
+            $('.navbar-brand').fadeIn('fast');
+        } else {
+            $('#main-menu').removeClass('navbar-fixed-top');
+            $('.main').removeAttr('style');
+            $('.navbar-brand').fadeOut('fast');
+        }
+    }
+
+    document.onscroll = scroll;
+});
